4e442cea5407c57303a55687b7c9c717c2fc8030
Carmel Lyza <148184575+Lyzaaaa21@users.noreply.github.com>
2024-09-04 18:37:51 +0800

    First Task sa Software Design
    First Task sa Software Design

---
+++ lyza/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Coffee from "./coffee";
+import Homepage from "./homepage";
+
+export default function Home() {
+  return (
+    <>
+    <Coffee />
+    <Homepage />
+    </>
+  );
+}
